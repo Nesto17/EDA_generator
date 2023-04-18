3e35c43eb211135236996521fdfcf812921b3424
Ernest Salim <salim.ernest17@gmail.com>
2023-04-18 16:35:36 -0700

    feat: base app

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("EDA Explorer")
+st.markdown("Welcome to **EDA Explorer**")
+
+if st.button("Say Hello!"):
+    st.write("Hellllooooooo")
+else:
+    st.write("Byeeeeeeee")
